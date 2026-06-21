53bb11de8c72f5bf375267564229392da70c92f4
Tamga <dev@tamga.local>
2026-06-21 20:34:03 +0300

    feat(sdk): add client SDK packages

---
+++ sdk/python/tamga/__init__.py
@@ -0,0 +1,4 @@
+from tamga.client import TamgaClient
+from tamga.errors import TamgaError
+
+__all__ = ["TamgaClient", "TamgaError"]
---
+++ sdk/typescript/tamga-client/src/index.ts
@@ -0,0 +1,2 @@
+export { TamgaClient } from './client.js';
+export { TamgaError } from './errors.js';
